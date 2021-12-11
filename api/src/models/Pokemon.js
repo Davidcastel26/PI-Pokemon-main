caeb8052d7f4cd4b1fea913cf34157691bc1e0c9
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-10 18:33:45 -0600

    createing the tables

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,50 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    hp:{
+      type: DataTypes.INTEGER
+    },
+    attack:{
+      type: DataTypes.INTEGER
+    },
+    defense:{
+      type: DataTypes.INTEGER
+    },
+    speed:{
+      type: DataTypes.INTEGER
+    },
+    height:{
+      type: DataTypes.INTEGER
+    }, 
+    weight:{
+      type: DataTypes.INTEGER
+    }
+  })
 };
+
+
+/*
+
+[ ] Pokemon con las siguientes propiedades:
+--------ID (Número de Pokemon) * : No puede ser un ID de un pokemon ya existente en la API pokeapi
+--------Nombre *
+--------Vida  (CALLED AS HP)
+--------Fuerza
+--------Defensa
+-------Velocidad
+------Altura
+------Peso
+[ ] Tipo con las siguientes propiedades:
+ID
+Nombre
+
+*/
