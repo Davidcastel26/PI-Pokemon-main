7aef23bf0ed07c4a89637c8f67c7dfde0a49ad30
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-12 14:36:08 -0600

    adding relation

--- api/src/db.js
+++ api/src/db.js
@@ -30,8 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type  } = sequelize.models;
 
+Pokemon.belongsToMany(Type,{
+  through: "PokemonsTypes"
+})
+Type.belongsToMany(Pokemon,{
+  through: "PokemonsTypes"
+})
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
