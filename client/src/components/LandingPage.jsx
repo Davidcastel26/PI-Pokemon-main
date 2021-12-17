f1550aeba9747a0a7fb58d771d693ee7eeace9db
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-17 16:10:17 -0600

    creating landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
+
+const LandingPage = props => {
+    return (
+        <div>
+            <h1>Hey! Welcome to the Poke world</h1>
+            <Link to ='/home'>
+                <button>HOME</button>
+            </Link>
+        </div>
+    )
+}
+
+LandingPage.propTypes = {
+
+}
+
+export default LandingPage
