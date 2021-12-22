5ac49750a57d014cecab63e1d8318176b7513831
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-22 15:10:53 -0600

    adding link h1 btn and handleclick

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import React,
 import { useDispatch,
          useSelector } from 'react-redux';
 import getCharacter from '../../actions';
+import {Link} from 'react-router-dom';
 
 const Home = () =>{
     const dispatch = useDispatch();
@@ -13,6 +14,20 @@ const Home = () =>{
         dispatch(getCharacter())
     },[])
 
+    const handleClick = ( e ) =>{
+        e.preventDefault();
+        dispatch(getCharacter)
+    }
+
+    return(
+        <div>
+            <Link to='/pokemon'>Create Pokemon</Link>
+            <h1>Pokemons</h1>
+            <button onClick={e=> handleClick(e)}>
+                reload all the pokemons
+            </button>
+        </div>
+    )
 }
 
 export default Home;
