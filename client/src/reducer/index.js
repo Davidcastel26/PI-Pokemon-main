e1bd6b5126806737ca7a4bf0f2f41b558a103ccc
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-27 16:04:38 -0600

    adding all new pokemons copy and sort by created and existing

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,6 +1,7 @@
 const initialState = {
     characters : [],
-    
+    //we are creating a copy so we won't filter into the array already filtered
+    AllCopyPokemons : []
 }
 
 const rootReducer = (state = initialState, action) =>{
@@ -11,13 +12,25 @@ const rootReducer = (state = initialState, action) =>{
         case 'GET_CHARACTERS':
             return{
                 ...state,
-                characters:action.payload
+                characters:action.payload,
+                AllCopyPokemons: action.payload
+            }
+        case 'GET_CHARACTER':
+            return{
+                ...state,
+                AllCopyPokemons : action.payload
             }
         case 'FILTER_BY_STRENGTH':
             const statusFilter = action.payload === 'All' ? allPokemons : allPokemons.filter(el => el.attack === action.payload)
             return{
                ...state,
                characters: statusFilter
+            }
+        case 'FILTER_BY_CREATEDOREXISTED':
+            const filterCE = action.payload === 'Created' ? state.AllCopyPokemons.filter(ele => ele.id.length > 8) : state.AllCopyPokemons.filter(ele => ele.id.length < 8)
+            return{
+                ...state,
+                characters : action.payload === 'All' ? state.characters : filterCE
             }
         case 'FILTER_BY_NAME':
             var sortByNmae;
