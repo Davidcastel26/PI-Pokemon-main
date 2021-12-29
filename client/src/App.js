ff30cbec7967c67d0ec3d691d6a432882ef6619b
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-29 17:53:49 -0600

    moving the file create into a own directory and rfce into file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import Home from './components/Home/Home'
+import CharacterCreate from './components/Create/CharacterCreate';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path='/' element={ <LandingPage/> }></Route>
+          <Route path='/pokemons' element={ <CharacterCreate/> } ></Route>
           <Route path='/home' element={ <Home/> }></Route>
         </Routes>
       </div>
---
+++ client/src/components/Create/CharacterCreate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+
+const CharacterCreate = () => {
+    return (
+        <div>
+            <h1>holi</h1>
+        </div>
+    )
+}
+
+export default CharacterCreate
