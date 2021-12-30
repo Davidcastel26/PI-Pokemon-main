684b7a2eb0abca2ed83c8d188b0657d9a0e94be0
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-30 16:42:29 -0600

    creating the bases

---
+++ client/src/components/Home/Detail/Detail.jsx
@@ -0,0 +1,16 @@
+//hooks
+import {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+// actions redux
+import { getDetail } from '../../../actions'
+
+const Detail = () => {
+    return (
+        <div>
+            holi
+        </div>
+    )
+}
+
+export default Detail
