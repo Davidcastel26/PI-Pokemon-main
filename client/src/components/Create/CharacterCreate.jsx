6192d95f7533c687fe8d26c40bdf7f75de9a6bd8
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-29 18:24:00 -0600

    importing the funcs from the action

--- client/src/components/Create/CharacterCreate.jsx
+++ client/src/components/Create/CharacterCreate.jsx
@@ -1,6 +1,10 @@
+// hooks from react 
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+// funcs from the action redux
+import { postPokemon, filterPokemonByType } from '../../actions'
+
 
 const CharacterCreate = () => {
     return (
