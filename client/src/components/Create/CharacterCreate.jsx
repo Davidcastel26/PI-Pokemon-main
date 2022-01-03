cd2e9c325291f5fa024d54bcbd5866eef280777c
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-03 16:13:38 -0600

    adding from controller

--- client/src/components/Create/CharacterCreate.jsx
+++ client/src/components/Create/CharacterCreate.jsx
@@ -12,15 +12,15 @@ const CharacterCreate = () => {
     const navigate = useNavigate();
     const types = useSelector(state => state.pokemonsTypes)
 
-    const [errors, setErrors] = useState({})
+    const [error, setErrors] = useState({})
     const [input, setInput] = useState({
         name:"",
-        hp: 0,
-        attack: 0,
-        defense:0,
-        speed:0, 
-        height:0,
-        weight:0,
+        hp: "",
+        attack: "",
+        defense:"",
+        speed:"", 
+        height:"",
+        weight:"",
         img:"",
         types:[]
     })
@@ -30,11 +30,41 @@ const CharacterCreate = () => {
     },[]);
 
     //---------- FORM CONTROLLER ----------------------
+
+    const checkInfoObj = obj => {
+        for( let e in obj){
+            if(obj[e] !== null && obj[e] != "") return false;
+        }
+        return true;
+    }
+
     const validate = input => {
         let errors = {};
-        if(!input.name){ errors.name = 'A name is needed :/'};
+
+        if(!input.name){ errors.name = 'A name is needed'};
         // if(!input.hp) { errors.name = 'A HP is needed :/'};
-        if(input.hp < 0){ errors.name = 'A HP should be more than 0 :/'}
+        
+        // if(input.hp < 0){ errors.name = 'A HP should be more than 0 :/'}
+        
+        if(!input.img){
+            errors.img = 'An URL is requiered';
+        }else if(!/^(ftp|http|https):\/\/[^ "]+$/.test(input.img)){
+            errors.img = 'A valid URL is needed exe. http://googlephotos.com'
+        }
+
+        if(!input.speed){
+            errors.speed = 'A Speed is required'
+        }else if(!/^[1-9]*$/.test(input.speed)){
+            errors.speed = 'A valid Speed is required, the weight could be until 98'
+        }
+
+        if(!input.weight){
+            errors.weight = 'A Weight is required'
+        }else if(!/^[1-9]*$/.test(input.weight)){
+            errors.weight = 'A valid Weight is required, the weight could be until 98'
+        }
+
+
 
         return errors;
     }
@@ -93,48 +123,48 @@ const CharacterCreate = () => {
                 <div>
                     <label htmlFor="">Name</label>
                     <input type="text"  value={input.name} name='name' onChange={e => handleChnage(e)}/>
-                    {!errors.name && (
-                        <p>{errors.name}</p>
-                    )
-                    }
+                    {error.name && (<p>{error.name}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">HP</label>
-                    <input type="text"  value={input.hp} name='hp' onChange={e => handleChnage(e)}/>
-                    {
-                        errors.hp && (
-                            <p>{errors.hp}</p>
-                        )
-                    }
+                    <input type="number"  value={input.hp} name='hp' onChange={e => handleChnage(e)}/>
+                    { error.hp && ( <p>{error.hp}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Attack</label>
-                    <input type="text"  value={input.attack} name='attack' onChange={e => handleChnage(e)}/>
-            
+                    <input type="number"  value={input.attack} name='attack' onChange={e => handleChnage(e)}/>
+                    { error.hp && ( <p>{error.hp}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Defense</label>
-                    <input type="text"  value={input.defense} name='defense' onChange={e => handleChnage(e)}/>
+                    <input type="number"  value={input.defense} name='defense' onChange={e => handleChnage(e)}/>
+                    { error.hp && ( <p>{error.hp}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Speed</label>
-                    <input type="text"  value={input.speed} name='speed' onChange={e => handleChnage(e)}/>
+                    <input type="number"  value={input.speed} name='speed' onChange={e => handleChnage(e)}/>
+                    { error.speed && ( <p>{error.speed}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Height</label>
-                    <input type="text"  value={input.height} name='height' onChange={e => handleChnage(e)}/>
+                    <input type="number"  value={input.height} name='height' onChange={e => handleChnage(e)}/>
+                    { error.height && ( <p>{error.height}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Weight</label>
-                    <input type="text"  value={input.weight} name='weight' onChange={e => handleChnage(e)}/>
+                    <input type="number"  value={input.weight} name='weight' onChange={e => handleChnage(e)}/>
+                    { error.weight && ( <p>{error.weight}</p>)}
+                    
                 </div>
                 <div>
                     <label htmlFor="">Image</label>
                     <input type="text" value={input.img} name='img' onChange={e => handleChnage(e)}/>
+                    { error.img && ( <p>{error.img}</p>)}
                 </div>
                 <div>
                     <label htmlFor="">Type</label>
                     <select name="" onChange={e=> handleSelector(e)} >
+                        <option hidden>none</option>
                         {types.map(typ => (
                             <option key={typ.id} value={typ.name}> 
                                 {typ.name} 
@@ -153,7 +183,7 @@ const CharacterCreate = () => {
                         </li>
                     </ul>
                 </div>
-                <button type='submit' > Create </button>
+                {(!Object.keys(error).length && !checkInfoObj(input)) ? (<button type='submit' > <span> Create </span> </button>) : <button disabled type='submit'> <span> Create </span></button>}
             </form>
         </div>
     )
