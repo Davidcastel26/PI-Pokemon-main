5f1214c81e9f70abd345bf197c984d17178f93df
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-03 23:20:39 -0600

    creating div and inserting the nav with h1

--- client/src/components/Landing/LandingPage.jsx
+++ client/src/components/Landing/LandingPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 //-----CSS
 import './landingpage.css'
 
 const LandingPage = () => {
     return (
-        <div>
-            <Link to ='/home'>
-                <button>HOME</button>
-            </Link>
-            <h1>Hey! Welcome to the Poke world</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <NavBar />
+                <h1>Hey! Welcome to the Poke world</h1>
+            </div>
         </div>
     )
 }
