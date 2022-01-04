febd2bae0cb01468dcde17e9c5e08469cb0ec85b
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-03 18:20:16 -0600

    cleaning the file

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 
 
-const LandingPage = props => {
+const LandingPage = () => {
     return (
         <div>
-            <h1>Hey! Welcome to the Poke world</h1>
             <Link to ='/home'>
                 <button>HOME</button>
             </Link>
+            <h1>Hey! Welcome to the Poke world</h1>
         </div>
     )
 }
