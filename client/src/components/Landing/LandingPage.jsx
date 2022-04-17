10fd9fea69e8aff2e3b772f0c6ab2269450503bc
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-17 16:43:26 -0600

    Update LandingPage.jsx

--- client/src/components/Landing/LandingPage.jsx
+++ client/src/components/Landing/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar';
+// import NavBar from '../NavBar/NavBar';
 //-----CSS
 import './landingpage.css'
 import landing from '../../imgs/pokemon.png'
