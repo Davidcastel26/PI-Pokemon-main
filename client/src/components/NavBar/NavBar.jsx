29919000d17f5d7da6625ac4fe6f8d0dde04584c
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-18 17:15:54 -0600

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -9,11 +9,14 @@ const NavBar = () => {
     return (
         <div className='gpt3__navbar'>
             <div className="gpt3__navbar-links">
+                
                 <div className='gpt3__navbar-links_logo'>
                     <Link to ='/'>
                         <img src={logo} alt="logo" />
                     </Link>
                 </div>
+
+
                 <div className='navbar-links_container'>
                     <Link to ='/'>
                         <p>Home</p>
@@ -25,6 +28,8 @@ const NavBar = () => {
                       <p>Create Pokemon</p>  
                     </Link>
                 </div>
+
+                
             </div>
         </div>
     )
